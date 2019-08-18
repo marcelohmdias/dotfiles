e9c323be8cfb7232d8eed918ec4b86dd21bd1576
marcelohmdias <marcelohmdias@gmail.com>
2019-08-18 11:40:56 -0300

    Adding config for Linux PPAs.

---
+++ src/ppas/ppas_apps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" && . "./../utils.sh"
+
+# -----------------------------------------------------------------------------
+
+main() {
+  local -r PPAS=( "lutris-team/lutris" )
+  local -r NAMES=( "Lutris" )
+
+  print_msg_sub_info "Apps"
+  run_list_ppas PPAS NAMES
+  break_line
+}
+
+# -----------------------------------------------------------------------------
+
+main
---
+++ src/ppas/ppas_tools.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" && . "./../utils.sh"
+
+# -----------------------------------------------------------------------------
+
+main() {
+  local -r PPAS=( "git-core/ppa" "dawidd0811/neofetch" )
+  local -r NAMES=( "Git" "Neofetch" )
+
+  print_msg_sub_info "Tools"
+  run_list_ppas PPAS NAMES
+  break_line
+}
+
+# -----------------------------------------------------------------------------
+
+main
+
