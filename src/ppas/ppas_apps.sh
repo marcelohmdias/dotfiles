740d598b9a3435edb1f3ed4f20a15800b6859757
marcelohmdias <marcelohmdias@gmail.com>
2019-09-21 17:49:52 -0300

    Adding Piper ppa.

--- src/ppas/ppas_apps.sh
+++ src/ppas/ppas_apps.sh
@@ -5,8 +5,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")" && . "./../utils.sh"
 # -----------------------------------------------------------------------------
 
 main() {
-  local -r PPAS=( "lutris-team/lutris" )
-  local -r NAMES=( "Lutris" )
+  local -r PPAS=( "lutris-team/lutris" "libratbag-piper/piper-libratbag-git" )
+  local -r NAMES=( "Lutris" "Piper" )
 
   print_msg_sub_info "Apps"
   run_list_ppas PPAS NAMES
