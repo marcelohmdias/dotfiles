f9912175a322c4c009da07d057c5672b5cf6473b
marcelohmdias <marcelohmdias@gmail.com>
2019-10-06 17:35:50 -0300

    Commando correction

--- src/setup.sh
+++ src/setup.sh
@@ -152,8 +152,9 @@ main() {
 
   execute "sudo rm /var/lib/apt/lists/lock &> /dev/null"        "Removing APT List Lock"
   execute "sudo rm /var/cache/apt/archives/lock &> /dev/null"   "Removing Cache APT Lock"
-  execute "sudo rm /var/lib/dpkg/lock-frontend &> /dev/null"    "Removing DPKG Lock File"
+  execute "sudo rm /var/lib/dpkg/lock* &> /dev/null"            "Removing DPKG Lock File"
   execute "sudo dpkg --configure -a &> /dev/null"               "Reconfiguring Packages"
+  break_line
 
   # -----------------------------------------------------------------------------
 
